c225356641ca406a8f2e7a7acdccc5db4c497963
kevtool <kevinlim9171@gmail.com>
2023-04-22 13:40:07 +0800

    Using RMarkdown for website

--- Blog/blog.py
+++ Blog/blog.py
@@ -2,18 +2,27 @@ from manim import *
 from manim.utils.color import Colors
 
 class Graph:
-    def __init__(self, scene, vertices_list):
+    def __init__(self, scene, vertices_list, edges_list):
         self.scene = scene
 
         # set up vertices
         self.vertices = VGroup()
         self.add_vertices(vertices_list) 
 
+        # set up lines
+        self.edges = VGroup()
+        self.add_edges(edges_list)
+
     # Given a list, add vertices accordingly
     def add_vertices(self, list):
         for rad, color, opac, x, y in list:
             v = Circle(radius=rad).set_fill(color, opacity=opac).shift(RIGHT*x + UP*y)
             self.vertices.add(v)
+
+    def add_edges(self, list):
+        for v1, v2 in list:
+            l = Line(self.vertices[v1], self.vertices[v2])
+            self.edges.add(l)
 
     # Simultaneously create all vertices
     def create_vertices(self):
@@ -22,6 +31,9 @@ class Graph:
             animations.append(Create(self.vertices[i]))
         self.scene.play(*animations)
 
+    # Show lines
+    def show_lines(self):
+        self.scene.play(FadeIn(self.lines))
 
 class Dijkstra(Scene):
     def construct(self):
@@ -33,6 +45,18 @@ class Dijkstra(Scene):
                         [0.3, WHITE, 1, 4.0, 0.3],
                         [0.3, WHITE, 1, 1.3, 2.5],
                         [0.3, WHITE, 1, -2.5, 1.5]]
+        
+        # edges list
+        graph1_elist = [[0,1],
+                        [0,2],
+                        [0,5],
+                        [1,2],
+                        [1,3],
+                        [2,3],
+                        [2,5],
+                        [3,4],
+                        [5,4]]
 
-        graph1 = Graph(self, graph1_vlist)
+        graph1 = Graph(self, graph1_vlist, graph1_elist)
         graph1.create_vertices()
+        graph1.show_lines()
